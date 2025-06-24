8ca744cd42b41f3eced2f2e4d7bb6ce529fd14b2
Ahmed-Ashraf98 <a.ashraf.elbendary@gmail.com>
2025-06-24 23:00:24 +0300

    Add chatbot input style

--- apps/SuperFitnessApp/src/app/features/chatbot/chatbotInput/chatbotInput.component.ts
+++ apps/SuperFitnessApp/src/app/features/chatbot/chatbotInput/chatbotInput.component.ts
@@ -3,10 +3,21 @@ import { CommonModule } from '@angular/common';
 import { IconFieldModule } from 'primeng/iconfield';
 import { InputIconModule } from 'primeng/inputicon';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-chatbot-input',
-  imports: [CommonModule, IconFieldModule, InputIconModule, InputTextModule],
+  imports: [
+    CommonModule,
+    IconFieldModule,
+    InputIconModule,
+    InputTextModule,
+    ButtonModule,
+  ],
   templateUrl: './chatbotInput.component.html',
   styleUrl: './chatbotInput.component.scss',
 })
-export class ChatbotInputComponent {}
+export class ChatbotInputComponent {
+  sendMessage() {
+    throw new Error('Method not implemented.');
+  }
+}
