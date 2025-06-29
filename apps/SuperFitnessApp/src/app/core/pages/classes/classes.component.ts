3ae3ff9f64622090a27beb875100103b56c08852
mahbahig <mahbahig2@gmail.com>
2025-06-29 03:36:32 +0300

    Created component for classes page + Added classes route

---
+++ apps/SuperFitnessApp/src/app/core/pages/classes/classes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-classes',
+  imports: [CommonModule],
+  templateUrl: './classes.component.html',
+  styleUrl: './classes.component.css',
+})
+export class ClassesComponent {}
