71f6d8b0b095db84aed63274a75bdb1e8193e130
Mohamedniem <engdida2020@gmail.com>
2025-07-01 00:18:51 +0300

    working in home page

--- apps/SuperFitnessApp/src/app/app.component.ts
+++ apps/SuperFitnessApp/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { FlowbiteService } from './core/services/flowbite.service';
+import { MainPageComponent } from "./shared/components/main-page/main-page.component";
+declare function initFlowbite(): void;
+
 
 @Component({
-  imports: [RouterModule, ButtonModule],
+  imports: [RouterModule, ButtonModule, MainPageComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SuperFitnessApp';
+
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(() => {
+      initFlowbite();
+    });
+  }
 }
