b281460be870756095886087c68910bd03b9128c
Seif Emam <seifmoataz27249@gmail.com>
2025-07-15 19:45:20 +0300

    feat: enhance user profile management and localization support
    - Introduced `UserProfileComponent` for user profile display and management.
    - Implemented `ThemeManagerService` and `TranslateManagerService` for theme and language toggling.
    - Added cookie management for user preferences using `CookiesManagerService`.
    - Integrated `ngx-translate` for localization with support for Arabic and English.
    - Created utility functions for app initialization and translation loading.
    - Updated authentication API service to include profile data retrieval and editing capabilities.
    - Added tests for new services and components to ensure functionality.

---
+++ apps/SuperFitnessApp/src/app/features/pages/user-profile/user-profile.component.ts
@@ -0,0 +1,53 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ThemeManagerService } from 'apps/SuperFitnessApp/src/app/core/services/ThemeManger/ThemeManagerService.service';
+import { TranslateManagerService } from 'apps/SuperFitnessApp/src/app/core/services/TranslateManger/translate-manager-service.service';
+import { AuthApiService } from 'apps/SuperFitnessApp/src/lib/auth-api/src/lib/auth-api.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+
+  constructor() { }
+  themeVal: boolean = false;
+  langVal: boolean = false;
+  fetchIsDone = false;
+
+  private readonly _themeManager = inject(ThemeManagerService); 
+  private readonly _translateManager = inject(TranslateManagerService);
+  private readonly _authApiService = inject(AuthApiService);
+
+
+  
+  toggleTheme() {
+    // this.themeVal = !this.themeVal;
+    console.log(this.themeVal);
+    this._themeManager.toggleTheme();
+  }
+  toggleLang() {
+    // this.langVal = this.langVal == 'ar' ? 'en' : 'ar';
+    console.log(`Language Now is :  ${this.langVal}`);
+    this._translateManager.toggleLanguage();
+  }
+
+  getUserPrefFromCookies() {
+    const theme = this._themeManager.getCurrentTheme();
+    const lang = this._translateManager.getCurrentLang();
+
+    if (theme == 'dark') {
+      this.themeVal = true;
+    }
+
+    if (lang == 'ar') {
+      this.langVal = true;
+    }
+  }
+
+  ngOnInit(): void {
+    this._themeManager.initTheme();
+    this.getUserPrefFromCookies();
+  }
+
+}
--- projects/auth-api/src/lib/adaptor/auth-login-api.adapter.ts
+++ projects/auth-api/src/lib/adaptor/auth-login-api.adapter.ts
@@ -1,4 +1,4 @@
-import { LoginAPIRes, LoginRes } from '../interface/loginRes';
+import { ProfileDataRes, LoginRes } from '../interface/loginRes';
 import { Injectable } from '@angular/core';
 import { Adapter } from '../interface/adapter';
 
@@ -7,11 +7,11 @@ import { Adapter } from '../interface/adapter';
 })
 export class AuthLoginAPIAdapter implements Adapter {
   constructor() {}
-  adapt(data: LoginAPIRes): LoginRes {
+  adapt(data: ProfileDataRes): LoginRes {
     return {
       message: data.message,
+      email: data.user.email,
       token: data.token,
-      email: data.email,
     };
   }
 }
