11318b4c989d4db15088b7f8a845a40a84279e95
Ahmed-Ashraf98 <a.ashraf.elbendary@gmail.com>
2025-07-18 13:26:16 +0300

    add trans files

--- apps/SuperFitnessApp/src/lib/auth-api/src/lib/enums/AuthAPI.endpoint.ts
+++ apps/SuperFitnessApp/src/lib/auth-api/src/lib/enums/AuthAPI.endpoint.ts
@@ -1,4 +1,4 @@
-import { environment } from './../../../../../../../../environments/environment';
+import { environment } from '../../../../../../../../environments/environment';
 
 export class AuthENDPOINT {
   static LOGIN = `${environment.baseUrl}/api/v1/auth/signin`;
