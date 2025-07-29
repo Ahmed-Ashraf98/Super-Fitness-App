81d53a82845fb9fe6cae62bec6f416b45225b64f
Ahmed-Ashraf98 <a.ashraf.elbendary@gmail.com>
2025-07-30 02:28:26 +0300

    create the custom card

--- apps/SuperFitnessApp/src/app/features/pages/home/home.component.ts
+++ apps/SuperFitnessApp/src/app/features/pages/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component } from '@angular/core';
-import { HeroComponent } from "./components/hero/hero.component";
+import { HeroComponent } from './components/hero/hero.component';
 import { AboutUsComponent } from '../about-us/about-us.component';
 import { WhyUsComponent } from './components/why-us/why-us.component';
 import { ChatbotFloatingComponent } from '../../chatbot/chatbotFloating/chatbotFloating.component';
+import { CustomCardComponent } from '../../../shared/components/cutom-card/custom-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [ HeroComponent ,AboutUsComponent,WhyUsComponent,ChatbotFloatingComponent],
+  imports: [
+    HeroComponent,
+    AboutUsComponent,
+    WhyUsComponent,
+    ChatbotFloatingComponent,
+    CustomCardComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
